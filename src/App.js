e8c76d4b452930af0af9997369c77ee5a2098153
verigin <verigin.aleks@gmail.com>
2018-03-19 00:22:31 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import DropDown from './DropDown';
+
+class App extends Component {
+  render() {
+    return (
+      <div>      
+        <DropDown/>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/DropDown.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class DropDown extends Component {
+    constructor (props) {
+        super (props);
+        this.state = { 'isOpened' : false, 'background' : 'white' };
+    }
+    toggleState () {
+        this.setState({ 'isOpened' : !this.state.isOpened })
+        //this.setState({ 'background' : 'black' })
+    }
+    render() {
+        console.log('isOpened', this.state.isOpened);
+        let dropdowntext;
+        if ( this.state.isOpened ) dropdowntext='isOpened';  
+    return (        
+        <div onClick={this.toggleState.bind(this)} >
+        DropDown
+        {dropdowntext}
+        </div>
+    )
+  }
+}
+
+export default DropDown;
