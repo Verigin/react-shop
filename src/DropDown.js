d7899c5654ab72fd0b12ca226f15ac3c5498f036
unknown <d:\mail_The Bat!>
2018-03-19 18:44:01 +0500

    part 5

--- src/DropDown.js
+++ src/DropDown.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component  } from 'react';
 
-class DropDown extends Component {
+class DropDown extends Component {    
     constructor (props) {
         super (props);
-        this.state = { 'isOpened' : false, 'background' : 'white' };
+        this.state = { email : ''};
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
     }
-    toggleState () {
-        this.setState({ 'isOpened' : !this.state.isOpened })
-        //this.setState({ 'background' : 'black' })
-    }
-    render() {
-        console.log('isOpened', this.state.isOpened);
-        console.log('menu', this.props.menu);
-        let dropdowntext;
-        if ( this.state.isOpened ) dropdowntext='isOpened';  
-    return (        
-        <div onClick={this.toggleState.bind(this)} >
-        {this.props.menu.map((menu, index) =>
-            <a href={menu.link} key={index}>{menu.label}</a>
-        )}
-        DropDown
-        {dropdowntext}
-        </div>
+    onSubmit (event) {
+        event.preventDefault();       
+        console.log(this.state.email);  
+    }    
+    onChange (event) {        
+        this.setState({email : event.target.value}); 
+    }  
+    render() {        
+        return (        
+            <div>
+                <form onSubmit={this.onSubmit}>
+                    <input placeholder="email" onChange={this.onChange}/> 
+                    <button >Ok</button>
+                </form>    
+            </div>
     )
   }
 }
