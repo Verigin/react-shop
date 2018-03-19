37d32a5c37bbaf2cf094b57ab5a3544642d4e632
verigin <verigin.aleks@gmail.com>
2018-03-20 01:21:37 +0500

    redux2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
-import './App.css';
-import DropDown from './DropDown';
-
-const menu = [
-  {
-  link : '/news/',
-  label : 'news'
-  }
-];
+import { connect } from 'react-redux';
 
 class App extends Component {
+  constructor (props) {
+    super (props);
+    this.state = { email : ''};
+    this.onChange = this.onChange.bind(this);
+  }
+  onChange (event) {        
+    // this.props.testStore.dispatch({type: 'ADD_TRACK', playload: event.target.value});
+  }  
   render() {
+    console.log("текущее состояние",this.props.testStore);
     return (
-      <div className="container">      
-        <DropDown menu={menu}/>
-      </div>
+      // <div className="container">      
+      //   <DropDown menu={menu}/>
+      // </div>   
+      <div>
+        <input type="text" />
+        <button /* onClick={this.onChange}*/>Add track</button>
+        <ul>
+          {this.props.testStore.map((item,index)=>
+            <li key={index}>{item}</li>
+          )}
+        </ul> 
+      </div> 
     )
   }
 }
 
-export default App;
+export default connect (
+  state => ({
+    testStore: state
+  }),
+  dispatch => ({})
+)(App);
--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@
 // registerServiceWorker();
 
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
 const store = createStore(playList);
 
 function playList(state = [],action) {
@@ -31,3 +36,10 @@ store.subscribe(() => {
 store.dispatch({type: 'ADD_TRACK', playload: 'Smells like spirit'});
 store.dispatch({type: 'ADD_TRACK', playload: 'yo!'});
 store.dispatch({type: 'ADD_TRACK', playload: 'yo!!!!'});
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>, 
+document.getElementById('root')
+);
